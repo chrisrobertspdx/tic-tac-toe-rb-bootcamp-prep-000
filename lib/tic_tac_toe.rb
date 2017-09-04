8a83b1abccbebd78f080e791272e63f919e8bf45
Chris Roberts <chrisrobertspdx@gmail.com>
2017-09-04 16:35:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,21 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space == 'X' || space == 'O'
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
