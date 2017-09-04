3912d35d3735261687b35db27b17250d87a85af5
Chris Roberts <chrisrobertspdx@gmail.com>
2017-09-04 16:36:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,3 +62,44 @@ def current_player(board)
     "O"
   end
 end
+
+def won?(board)
+  players = ["X","O"]
+  players.each do |player|
+    WIN_COMBINATIONS.each do |combination|
+      if board[combination[0]] == player && board[combination[1]] == player && board[combination[2]] == player
+        return combination
+      end
+    end
+  end
+  nil
+end
+
+def full?(board)
+  board.all? do |cell|
+    !(cell.nil? || cell == " ")
+  end
+end
+
+def draw?(board)
+  if won?(board) != nil
+    false
+  elsif !full?(board)
+    false
+  else
+    true
+  end
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  winpos = won?(board)
+  if winpos
+    board[winpos[0]]
+  else
+    winpos
+  end
+end
